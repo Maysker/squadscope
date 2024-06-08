4a8a3c3e7483903516032d65be3f3df133f3023f
AntonioMaggi <maggia1013@gmail.com>
2024-06-08 15:13:51 +0200

    added basic registration and login with modals

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    
     
     @include('layout.header')  
 </head>
@@ -20,6 +24,8 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
     @include('layout.footer')  
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register.show');
+Route::post('/register', [UserController::class, 'store'])->name('register.store');
+Route::post('/login-submit', [UserController::class, 'login'])->name('login.submit');
+Route::get('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
     return view('welcome');
