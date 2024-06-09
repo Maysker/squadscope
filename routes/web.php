fbdddfbb0a2ceb23ba40c2a14b1505cdc92746df
Maysker <a.gazdiev@gmail.com>
2024-06-09 23:30:42 +0200

    Added route for checking players in PlayerController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
 
+Route::post('/check-players', [PlayerController::class, 'checkPlayers']);
 Route::get('/', function () {
     return view('welcome');
 });
